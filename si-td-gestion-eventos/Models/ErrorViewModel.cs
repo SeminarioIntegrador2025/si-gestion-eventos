4d26b425e3c6470d0e4bd984734ee92ef1928b6b
luciact <luciactommasi@gmail.com>
2025-09-29 22:24:19 -0300

    Agregar archivos de proyecto.

---
+++ si-td-gestion-eventos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace si_td_gestion_eventos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
