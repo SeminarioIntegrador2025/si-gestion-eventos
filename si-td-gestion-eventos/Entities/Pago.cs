213715e1cffdd317bc2a77b02e82fcfee163217a
NicolasMullerr <nicolasmuller1538@gmail.com>
2025-12-08 11:57:37 -0300

    Cambios menores

--- si-td-gestion-eventos/Entities/Pago.cs
+++ si-td-gestion-eventos/Entities/Pago.cs
@@ -9,11 +9,8 @@ namespace si_td_gestion_eventos.Entities
         public float Monto { get; set; }
         public MetodoPago Metodo { get; set; }
         public string? Observaciones { get; set; }
-
-        // --- ARREGLO 1: Propiedades de Relación ---
-        public int EventoId { get; set; } // <-- La propiedad que faltaba
+        public int EventoId { get; set; }
         public Evento Evento { get; set; }
-
         public int? ComprobanteExternoId { get; set; } // Para la relación 1-a-1
         public ComprobanteExterno? ComprobanteExterno { get; set; }
     }
